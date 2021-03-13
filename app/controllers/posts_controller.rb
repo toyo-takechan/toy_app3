5a7bd4a84f3e095109f90675127f466741d8641d
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-03-13 19:58:07 +0900

    post create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,34 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+      if @post.save
+        flash[:info] ="投稿に成功しました。" 
+        redirect_to root_url     
+      else
+       render 'new'
+      end
   end
 
   def show
+    @post = Post.find_by()
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content )
   end
 end
