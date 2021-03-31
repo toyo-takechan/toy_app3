d373673c189b05f762435dfa543fb388c9680a48
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-03-31 13:01:02 +0900

    headerloginlogout

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-// #追記
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
@@ -8,5 +7,5 @@ environment.plugins.prepend('Provide',
     jQuery: 'jquery/src/jquery'
   })
 )
-// #ここまで
+
 module.exports = environment
