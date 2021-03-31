23dee4e672c98bc7ef4a246d43e8711ef73eb65c
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-01 00:19:14 +0900

    rememberdigestmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_040842) do
+ActiveRecord::Schema.define(version: 2021_03_31_110040) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_03_21_040842) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
