60f0fac0e6af6b06cd4b6df6c46722261dac9b23
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-02 14:28:29 +0900

    authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def index
     @users = User.all
   end
@@ -27,7 +30,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:sucsess] = "プロフィールを更新しました。"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+
+  end
+
   def destroy
+    User.find(params[:id]).destroy
+    flash[:sucsess] = "ユーザーを削除しました。"
+    redirect_to users_url
   end
 
   private
@@ -36,5 +53,21 @@ class UsersController < ApplicationController
                      :password_confirmation)
   end
 
+
+  # beforeアクション
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
 end
